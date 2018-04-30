9b93abcccda6ed4a3860f08d2f18e209b30dcca8
jugonzalez40 <jugonzalez40@poligran.edu.co>
2018-04-30 15:11:34 -0500

    End Backup

--- src/jobs/JSendSms.js
+++ src/jobs/JSendSms.js
@@ -1,7 +1,7 @@
 const CronJob = require('cron').CronJob;
 const LOGGER = require('../core/log')
 const JOBS_CONFIG = require('../../config/jobs.config.json');
-
+const Sms = require('../core/sms')
 
 class JSendSms {
 
@@ -11,21 +11,48 @@ class JSendSms {
     this.start = this.start.bind(this);
     this.onTick = this.onTick.bind(this);
     this.onComplete = this.onComplete.bind(this);
+    this.job = new CronJob(this.JOB_CONFIG.cron, this.onTick, this.onComplete);
   }
 
   start() {
     try {
-      LOGGER.INFO(`${this.constructor.name} init`);
-      new CronJob(this.JOB_CONFIG.cron, this.onTick, this.onComplete).start();
+      this.job.start();
+      LOGGER.INFO(`${this.constructor.name} job has init successfully 👏👏👏👏👏`);
     } catch (error) {
-      LOGGER.ERROR(`Invalid cron ${error}`);
+      LOGGER.ERROR(`Invalid cron => ${error}`);
     }
+  }
+
+  chooseMessage() {
+    let index = Math.floor(Math.random() * this.JOB_CONFIG.messages.length);
+    return this.JOB_CONFIG.messages[index];
+  }
+
+  isBlackHour(date = new Date()) {
+    return this.JOB_CONFIG.blackHours
+      .some(blackHour => {
+        let gH = date.getHours();
+        return blackHour === gH;
+      });
+  }
+
+  getNextDateTick() {
+    let nextDates = this.job.nextDates(2); //array
+    let isBH = this.isBlackHour(nextDates[0]._d);
+    console.log(nextDates[0]._d,nextDates[1]._d)
+    return isBH ? nextDates[1]._d : nextDates[0]._d;
   }
 
   //@overide implenets JOB
   onTick(job) {
-    LOGGER.INFO(`${this.constructor.name} tick`);
+    /*if (this.isBlackHour()) return;
+    let message = this.chooseMessage();
+    new Sms().send(message);
+    LOGGER.INFO(`${this.constructor.name} => 🥕 tick => ${message} => Next tick at ${this.getNextDateTick()} `);
+    console.dir(this.job.nextDates());*/
+    this.getNextDateTick();
   }
+
   onComplete(job) {
     LOGGER.INFO(`${this.constructor.name} completed`);
   }
@@ -33,22 +60,3 @@ class JSendSms {
 
 
 module.exports = JSendSms;
-/*
-module.exports = {
-  start: (smsInstance) => {
-    logger.info('---------------------------------------------------JOB EXXECUTE');
-
-    var j = schedule.scheduleJob(, function () {
-      console.log('??')
-      let horaActual = +(new Date().getHours());
-
-      if (!(horaActual >= 21 && horaActual <= 23) || !(horaActual >= 0 && horaActual <= 8)) {
-        //smsInstance.send('¡Es hora de comer, negra!');
-        logger.info('Se ha enviado' + (new Date).toString());
-      } else {
-        logger.info('No se ha enviado ' + (new Date()).toString());
-      }
-
-    });
-  }
-}*/
